03a9ce94ca87c46b63fd264168b7987b25e64652
gusev_ae <gusev_ae@magnit.ru>
2025-10-22 01:33:34 +0300

    подключил библиотеку lombok. переписал файлы Pojo.

--- src/main/java/praktikum/order/Order.java
+++ src/main/java/praktikum/order/Order.java
@@ -1,16 +1,12 @@
 package praktikum.order;
 
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-
-import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.notNullValue;
-
+@Getter
+@RequiredArgsConstructor
 public class Order {
-
     private final String firstName;
     private final String lastName;
     private final String address;
@@ -20,58 +16,4 @@ public class Order {
     private final String deliveryDate;
     private final String comment;
     private final List<String> color;
-
-    public Order(String firstName, String lastName, String address, String metroStation, String phone, int rentTime, String deliveryDate, String comment, List<String> color) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.metroStation = metroStation;
-        this.phone = phone;
-        this.rentTime = rentTime;
-        this.deliveryDate = deliveryDate;
-        this.comment = comment;
-        this.color = color;
-    }
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getMetroStation() {
-        return metroStation;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public int getRentTime() {
-        return rentTime;
-    }
-
-    public String getDeliveryDate() {
-        return deliveryDate;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public List<String> getColor() {
-        return color;
-    }
-
-
-
-
-
-
-
 }
