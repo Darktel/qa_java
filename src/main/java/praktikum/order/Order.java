058f81919b8a8d4a94c440bb53dad1edb101db40
gusev_ae <gusev_ae@magnit.ru>
2025-10-22 01:17:41 +0300

    Изменил порядок структуры в POJO файле Order.java

--- src/main/java/praktikum/order/Order.java
+++ src/main/java/praktikum/order/Order.java
@@ -10,6 +10,28 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class Order {
+
+    private final String firstName;
+    private final String lastName;
+    private final String address;
+    private final String metroStation;
+    private final String phone;
+    private final int rentTime;
+    private final String deliveryDate;
+    private final String comment;
+    private final List<String> color;
+
+    public Order(String firstName, String lastName, String address, String metroStation, String phone, int rentTime, String deliveryDate, String comment, List<String> color) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.metroStation = metroStation;
+        this.phone = phone;
+        this.rentTime = rentTime;
+        this.deliveryDate = deliveryDate;
+        this.comment = comment;
+        this.color = color;
+    }
     public String getFirstName() {
         return firstName;
     }
@@ -46,27 +68,7 @@ public class Order {
         return color;
     }
 
-    private final String firstName;
-    private final String lastName;
-    private final String address;
-    private final String metroStation;
-    private final String phone;
-    private final int rentTime;
-    private final String deliveryDate;
-    private final String comment;
-    private final List<String> color;
 
-    public Order(String firstName, String lastName, String address, String metroStation, String phone, int rentTime, String deliveryDate, String comment, List<String> color) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.metroStation = metroStation;
-        this.phone = phone;
-        this.rentTime = rentTime;
-        this.deliveryDate = deliveryDate;
-        this.comment = comment;
-        this.color = color;
-    }
 
 
 
